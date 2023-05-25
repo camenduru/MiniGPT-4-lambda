d74ba99e015f945c68714d7232e51c5427a127e9
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-26 02:25:01 +0300

    test

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ os.system(f"git lfs install")
 os.system(f"git clone -b dev https://github.com/camenduru/minigpt4 /home/demo/source/minigpt4")
 os.chdir(f"/home/demo/source/minigpt4")
 
-os.system(f"https://huggingface.co/ckpt/minigpt4/resolve/main/minigpt4.pth -O /home/demo/source/minigpt4/checkpoint.pth")
-os.system(f"https://huggingface.co/ckpt/minigpt4/resolve/main/blip2_pretrained_flant5xxl.pth -O /home/demo/source/minigpt4/blip2_pretrained_flant5xxl.pth")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/minigpt4/resolve/main/minigpt4.pth -d /home/demo/source/minigpt4 -o checkpoint.pth")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/minigpt4/resolve/main/blip2_pretrained_flant5xxl.pth -d /home/demo/source/minigpt4 -o blip2_pretrained_flant5xxl.pth")
 
 os.system(f"pip install -r requirements.txt")
 os.system(f"python app.py")
